a2aedaabd264f377733d8da85179ff737dcd5db1
PRATHAMESHATKARE <142227723+prathameshatkare@users.noreply.github.com>
2025-09-01 15:30:37 +0530

    Add files via upload

---
+++ MISSION 25/DEV/JAVASCRIPT/DAY_04/index.js
@@ -0,0 +1,37 @@
+"use strict";//my file is in new format of javascript
+
+// console.alert("hello")--->not work because we are working on the node environment not on browser
+
+
+
+let number =123//number
+
+console.log(name)
+console.log(number)
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 +3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
---
+++ MISSION 25/DEV/JAVASCRIPT/Day_03/03_Variables.js
@@ -0,0 +1,10 @@
+const accountEmail="prathmeshatkare07@gmail.com"
+let accountName="prathamesh"
+var accountNumber=9604011485
+accountPassword=12121
+let accountstate;
+
+console.table([accountEmail,accountName,accountNumber,accountPassword,accountstate])
+/*
+Prefer Not to use Var for blockscope and functional scope
+*/
